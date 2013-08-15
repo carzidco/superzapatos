83b80b2b76f5a356ba278358bc76463df2386b83
Roy Rivera <roy.fita23@gmail.com>
2013-08-15 15:12:14 -0500

    Initial Structure

---
+++ superzapatos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Superzapatos::Application.config.secret_token = 'a80846f08294fe343a6ab3dc5a396a4b44b0bb8f6553c7edc0a480b6dc0c5f384e34a171e732d147d628606c8fac0aa6d172349ac3ed1e903219cca30380fa3e'
